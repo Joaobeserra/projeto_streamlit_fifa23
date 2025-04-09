9ddbc5a5ea5db366e5cf74b6dda10e04583f85de
Joao vitor Beserra <JoaoVitorBeserra@hotmail.com>
2025-04-09 15:34:22 -0400

    att

--- 1_home.py
+++ 1_home.py
@@ -2,11 +2,14 @@ import streamlit as st
 import webbrowser
 import pandas as pd
 from datetime import datetime
+import os
+
 
 st.set_page_config(page_title="Home", page_icon="🏠", layout="wide")
 
 if "data" not in st.session_state:
-    df_data = pd.read_csv("dataset\\CLEAN_FIFA23_official_data.csv", index_col=0)
+    caminho_arquivo = os.path.join("dataset", "CLEAN_FIFA23_official_data.csv")
+    df_data = pd.read_csv(caminho_arquivo, index_col=0)
     df_data = df_data[df_data["Contract Valid Until"] >= datetime.today().year]
     df_data = df_data[df_data["Value(£)"] > 0]
     df_data = df_data.sort_values(by="Overall", ascending=False)
